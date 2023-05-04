dff01a3267de667e13f66ad486a9c3da25e77a4a
mdrahib46 <nahid179846@bsdi-bd.org>
2023-05-04 20:17:21 +0600

    Solve 5 dart problems by using switch case in

--- bin/switch case.dart
+++ bin/switch case.dart
@@ -1,15 +1,15 @@
-import 'dart:convert';
+
 import 'dart:io';
 
-void main(){
+
+void main() {
 
   // Write a dart program to calculate the day of week.
-
   int? currentDay;
   stdout.write("Enter a number between (1-7) :");
   currentDay = int.parse(stdin.readLineSync()!);
 
-  switch(currentDay){
+  switch (currentDay) {
     case 7:
       print("Friday");
       break;
@@ -36,13 +36,13 @@ void main(){
   }
 
 
-  // Write a dart program to find out total number of days in a months
 
+  // Write a dart program to find out total number of days in a months
   int? numberOfMonth;
   stdout.write("Enter a number between (1-12) :");
   numberOfMonth = int.parse(stdin.readLineSync()!);
-  
-  switch(numberOfMonth){
+
+  switch (numberOfMonth) {
     case 1:
       print('Total number of days = 31');
       break;
@@ -80,9 +80,62 @@ void main(){
       print('Total number of days = 31');
       break;
     default:
-      print('Invalid input! Please enter month number between 1-12' );
+      print('Invalid input! Please enter month number between 1-12');
   }
 
 
+  // Write a dart program to check whether an alphabet is vowel or consonant using switch case.
+  String? ch;
+  stdout.write("Enter a alphabet :");
+  ch = stdin.readLineSync()!;
 
+  switch (ch) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+      print("vowel");
+      break;
+         default:
+      print("Consonant");
+  }
+
+  // Write a dart program to find maximum between two numbers using switch case.
+  int? number1;
+  stdout.write("Input Number 1 :");
+  number1 = int.parse(stdin.readLineSync()!);
+  int? number2;
+  stdout.write("Input Number 2 :");
+  number2 = int.parse(stdin.readLineSync()!);
+  switch(number1 > number2){
+    case true:
+      print('$number1 is maximum !');
+      break;
+    case false:
+      print('$number2 is maximum !');
+      break;
+
+  }
+
+  // write a dart program  to check even or odd using
+  int? number;
+  stdout.write("Input Number :");
+  number = int.parse(stdin.readLineSync()!);
+
+  switch(number % 2){
+    case 0:
+      print("number is even");
+      break;
+    case 1:
+      print('number is odd');
+      break;
+  }
 }
+
+
